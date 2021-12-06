6bc3e121993ce24424621701486822ea288b0dba
Fabrizio Parodi <fabrizio.parodi@ge.infn.it>
2021-12-06 10:20:22 +0100

    update

--- ps.py
+++ ps.py
@@ -2,12 +2,12 @@
 
 from   pyvisa import ResourceManager
 
-def init:
+def psinit():
     rm = ResourceManager()
     instr = rm.open_resource("USB0::0x0AAD::0x0135::035375056::INSTR")
     return instr
 
-def sel(instr,ch):
+def pssel(instr,ch):
     instsel = f"INST: NSEL {ch}"
     instout = f"INST OUT{ch}"
     instr.write(instsel)
